b3dd1a1a31a25d5cef2e4b134779b8ec55a593ae
Vigneshwar_V <haivicky@gmail.com>
2017-04-17 16:30:16 +0530

    webhook donefor messenger bot

--- server/IPLFantasyAssistant/app.js
+++ server/IPLFantasyAssistant/app.js
@@ -24,6 +24,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.get('/webhook', function (req, res) {
+if (req.query['hub.verify_token'] === 'IPL_Fantasy_BOT_VERIFY_TOKEN') {
+  res.send(req.query['hub.challenge']);
+} else {
+  res.send('Error, wrong validation token');    
+}
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
