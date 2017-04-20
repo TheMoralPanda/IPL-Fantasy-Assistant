4c8b43d26e3c3cb8cc624a1c7af64e4815ce99d6
Vigneshwar_V <haivicky@gmail.com>
2017-04-20 18:53:04 +0530

    App message not receiving. Bot code begin

--- server/IPLFantasyAssistant/app.js
+++ server/IPLFantasyAssistant/app.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+var webhook = require('./routes/webhook');
+const token = "EAAaSG2aET7wBAOxs7wiCqYF52jYX7lpsnyUEBTP2FtHSIVo4n85fiCvh0kzNNO8QY9YHVxZCChX7ZBmeHzlO2pFpbCmC7yt9LqSTHDRN5pDBBD0Dtqc1tr1XdwDemek2W9nQx0Wkm0ZBZB55lKkMdhgLZAiX7hoBD3szcuicYLAZDZD";
 var app = express();
 
 // view engine setup
@@ -24,14 +25,48 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.get('/webhook', function (req, res) {
-if (req.query['hub.verify_token'] === 'IPL_Fantasy_BOT_VERIFY_TOKEN') {
-  res.send(req.query['hub.challenge']);
-} else {
-  res.send('Error, wrong validation token');    
-}
-});
 
+
+/* GET users listing. */
+app.get('/webhook', function (req, res) {
+	if (req.query['hub.verify_token'] === 'IPL_Fantasy_BOT_VERIFY_TOKEN') {
+	  res.send(req.query['hub.challenge']);
+	} else {
+	  res.send('Error, wrong validation token');    
+	}
+});
+app.post('/webhook', function(req, res){
+	var msg_events = req.body.entry[0].messaging
+	for(var i =0;i<msg_events.length;i++){
+		var event = req.body.entry[0].messaging[i]
+		var sender = event.sender.id
+		if(event.message && event.message.text){
+			var text = event.message.text
+			sendTextMessage(sender, "Text received, echo: "+text.substring(0,200))
+		}
+	}
+	res.sendStatus(200)
+
+})
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:token},
+	    method: 'POST',
+		json: {
+		    recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+		    console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+	    }
+    })
+}
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
