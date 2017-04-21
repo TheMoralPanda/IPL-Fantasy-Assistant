b2b26f8d5a96e8c9cb7e86d776b11cd7a73d0fa7
Vigneshwar <haivicky@gmail.com>
2017-04-21 19:50:40 +0530

    Basic bot is ready. Bot is echoing message

--- server/IPLFantasyAssistant/app.js
+++ server/IPLFantasyAssistant/app.js
@@ -1,88 +1,134 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+//This is still work in progress
+/*
+Please report any bugs to nicomwaks@gmail.com
+i have added console.log on line 48 
+ */
+'use strict'
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var webhook = require('./routes/webhook');
-const token = "EAAaSG2aET7wBAOxs7wiCqYF52jYX7lpsnyUEBTP2FtHSIVo4n85fiCvh0kzNNO8QY9YHVxZCChX7ZBmeHzlO2pFpbCmC7yt9LqSTHDRN5pDBBD0Dtqc1tr1XdwDemek2W9nQx0Wkm0ZBZB55lKkMdhgLZAiX7hoBD3szcuicYLAZDZD";
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+var app = express()
 
 
-/* GET users listing. */
-app.get('/webhook', function (req, res) {
-	if (req.query['hub.verify_token'] === 'IPL_Fantasy_BOT_VERIFY_TOKEN') {
-	  res.send(req.query['hub.challenge']);
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// index
+app.get('/', function (req, res) {
+	res.send('hello world i am a secret bot')
+})
+
+// for facebook verification
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+		res.send(req.query['hub.challenge'])
 	} else {
-	  res.send('Error, wrong validation token');    
+		res.send('Error, wrong token')
 	}
-});
-app.post('/webhook', function(req, res){
-	var msg_events = req.body.entry[0].messaging
-	for(var i =0;i<msg_events.length;i++){
-		var event = req.body.entry[0].messaging[i]
-		var sender = event.sender.id
-		if(event.message && event.message.text){
-			var text = event.message.text
-			sendTextMessage(sender, "Text received, echo: "+text.substring(0,200))
+})
+
+// to post data
+app.post('/webhook/', function (req, res) {
+	let messaging_events = req.body.entry[0].messaging
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = req.body.entry[0].messaging[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+			let text = event.message.text
+			if (text === 'Generic'){ 
+				console.log("welcome to chatbot")
+				sendGenericMessage(sender)
+				continue
+			}
+			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+		}
+		if (event.postback) {
+			let text = JSON.stringify(event.postback)
+			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+			continue
 		}
 	}
 	res.sendStatus(200)
-
 })
 
+
+// recommended to inject access tokens as environmental variables, e.g.
+// const token = process.env.FB_PAGE_ACCESS_TOKEN
+const token = "EAAaSG2aET7wBAKFHp4la422JXJwZCzZA7wZCEnMosOfHMdqs9YA8FC6kgKwK9kXOhJjNPEXtUurxLWqOpCYqAwCsuJ6sFFKOfz9eW37dePzeYgp5vCucci8X1CndiNA9jJDmggreqwww3GUutmqwXMjDG2EiXKSUeWu7m9aQgZDZD"
+
 function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-	    url: 'https://graph.facebook.com/v2.6/me/messages',
-	    qs: {access_token:token},
-	    method: 'POST',
+	let messageData = { text:text }
+	
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
 		json: {
-		    recipient: {id:sender},
+			recipient: {id:sender},
 			message: messageData,
 		}
 	}, function(error, response, body) {
 		if (error) {
-		    console.log('Error sending messages: ', error)
+			console.log('Error sending messages: ', error)
 		} else if (response.body.error) {
-		    console.log('Error: ', response.body.error)
-	    }
-    })
+			console.log('Error: ', response.body.error)
+		}
+	})
 }
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+function sendGenericMessage(sender) {
+	let messageData = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "generic",
+				"elements": [{
+					"title": "First card",
+					"subtitle": "Element #1 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+					"buttons": [{
+						"type": "web_url",
+						"url": "https://www.messenger.com",
+						"title": "web url"
+					}, {
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for first element in a generic bubble",
+					}],
+				}, {
+					"title": "Second card",
+					"subtitle": "Element #2 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+					"buttons": [{
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for second element in a generic bubble",
+					}],
+				}]
+			}
+		}
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
 
 module.exports = app;
+app.listen(4000); //the port you want to use
